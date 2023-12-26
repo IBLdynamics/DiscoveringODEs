38b911c0aedee52ca359c29ac82f72762b8454f5
jford1906 <jford1906@gmail.com>
2023-12-26 10:16:42 -0600

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,230 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter-2",
+  "level": "1",
+  "url": "frontmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  example.org   copyright  "
+},
+{
+  "id": "Autonomous_v_non",
+  "level": "1",
+  "url": "Autonomous_v_non.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Autonomous vs Non-autonomous",
+  "body": " Autonomous vs Non-autonomous   Straight line, the best kind of line    The Archimedian Spiral ,  Archimedian Spiral      Test exploration      Test conjecture      Test application      Test proof    "
+},
+{
+  "id": "Autonomous_v_non-3",
+  "level": "2",
+  "url": "Autonomous_v_non.html#Autonomous_v_non-3",
+  "type": "Figure",
+  "number": "1.1.1",
+  "title": "",
+  "body": " The Archimedian Spiral ,  Archimedian Spiral   "
+},
+{
+  "id": "expl-11",
+  "level": "2",
+  "url": "Autonomous_v_non.html#expl-11",
+  "type": "Exploration",
+  "number": "1.1.1",
+  "title": "",
+  "body": "  Test exploration   "
+},
+{
+  "id": "conj-11",
+  "level": "2",
+  "url": "Autonomous_v_non.html#conj-11",
+  "type": "Conjecture",
+  "number": "1.1.2",
+  "title": "",
+  "body": "  Test conjecture   "
+},
+{
+  "id": "app-11",
+  "level": "2",
+  "url": "Autonomous_v_non.html#app-11",
+  "type": "Application",
+  "number": "1.1.3",
+  "title": "",
+  "body": "  Test application   "
+},
+{
+  "id": "pf-11",
+  "level": "2",
+  "url": "Autonomous_v_non.html#pf-11",
+  "type": "Proof",
+  "number": "1.1.4",
+  "title": "",
+  "body": "  Test proof   "
+},
+{
+  "id": "SlopeFields",
+  "level": "1",
+  "url": "SlopeFields.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Slope Fields",
+  "body": " Slope Fields  Whatever   Exploring a slope field  Exploring a slope field     Explore    Conjecture    Apply    Prove   "
+},
+{
+  "id": "SlopeFields-3",
+  "level": "2",
+  "url": "SlopeFields.html#SlopeFields-3",
+  "type": "Figure",
+  "number": "1.2.1",
+  "title": "",
+  "body": " Exploring a slope field  Exploring a slope field   "
+},
+{
+  "id": "ModellingBasics",
+  "level": "1",
+  "url": "ModellingBasics.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "Modelling Basics",
+  "body": " Modelling Basics   Straight line, the best kind of line    Explore    Conjecture    Apply    Prove   "
+},
+{
+  "id": "GeometricDefinitions",
+  "level": "1",
+  "url": "GeometricDefinitions.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Geometric definition and relation to slope field",
+  "body": " Geometric definition and relation to slope field   Straight line, the best kind of line    Explore    Conjecture    Apply    Prove   "
+},
+{
+  "id": "VerifyingSolutions",
+  "level": "1",
+  "url": "VerifyingSolutions.html",
+  "type": "Section",
+  "number": "2.2",
+  "title": "Determining whether a given function satisfies the ODE",
+  "body": " Determining whether a given function satisfies the ODE   Straight line, the best kind of line    Explore    Conjecture    Apply    Prove   "
+},
+{
+  "id": "AutonomousODES",
+  "level": "1",
+  "url": "AutonomousODES.html",
+  "type": "Section",
+  "number": "2.3",
+  "title": "Autonomous ODEs: equilibria and phase lines",
+  "body": " Autonomous ODEs: equilibria and phase lines   Straight line, the best kind of line    Explore    Conjecture    Apply    Prove   "
+},
+{
+  "id": "OneODEManySolutions",
+  "level": "1",
+  "url": "OneODEManySolutions.html",
+  "type": "Section",
+  "number": "2.4",
+  "title": "One ODE, many solutions",
+  "body": " One ODE, many solutions   Straight line, the best kind of line    Explore    Conjecture    Apply    Prove   "
+},
+{
+  "id": "SolvingLinear",
+  "level": "1",
+  "url": "SolvingLinear.html",
+  "type": "Section",
+  "number": "2.5",
+  "title": "Solving <span class=\"process-math\">\\(x' = ax \\)<\/span>",
+  "body": " Solving   Straight line, the best kind of line    Explore    Conjecture    Apply    Prove   "
+},
+{
+  "id": "SecondOrderExamples",
+  "level": "1",
+  "url": "SecondOrderExamples.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Second order ODEs",
+  "body": " Second order ODEs   Straight line, the best kind of line    Graphing a second order ODE  Graphing a 2nd order ode      Explore    Conjecture    Apply    Prove   "
+},
+{
+  "id": "SecondOrderExamples-3",
+  "level": "2",
+  "url": "SecondOrderExamples.html#SecondOrderExamples-3",
+  "type": "Figure",
+  "number": "3.1.1",
+  "title": "",
+  "body": " Graphing a second order ODE  Graphing a 2nd order ode    "
+},
+{
+  "id": "Laplace",
+  "level": "1",
+  "url": "Laplace.html",
+  "type": "Chapter",
+  "number": "4",
+  "title": "Laplace Transforms",
+  "body": " Laplace Transforms   Hey kids, want to see an ODE?  It's not so scary if you poke it with a stick.   "
+},
+{
+  "id": "SystemsOfODEs",
+  "level": "1",
+  "url": "SystemsOfODEs.html",
+  "type": "Chapter",
+  "number": "5",
+  "title": "Systems of ODEs",
+  "body": " Systems of ODEs   Hey kids, want to see an ODE?  It's not so scary if you poke it with a stick.   "
+},
+{
+  "id": "SeriesSolutions",
+  "level": "1",
+  "url": "SeriesSolutions.html",
+  "type": "Chapter",
+  "number": "6",
+  "title": "Series Solutions to ODEs",
+  "body": " Series Solutions to ODEs   Hey kids, want to see an ODE?  It's not so scary if you poke it with a stick.   "
+},
+{
+  "id": "BVP",
+  "level": "1",
+  "url": "BVP.html",
+  "type": "Chapter",
+  "number": "7",
+  "title": "Boundary Value Problems",
+  "body": " Boundary Value Problems   Hey kids, want to see an ODE?  It's not so scary if you poke it with a stick.   "
+},
+{
+  "id": "Fourier",
+  "level": "1",
+  "url": "Fourier.html",
+  "type": "Chapter",
+  "number": "8",
+  "title": "Fourier Analysis",
+  "body": " Fourier Analysis   Hey kids, want to see an ODE?  It's not so scary if you poke it with a stick.   "
+},
+{
+  "id": "HigherOrder",
+  "level": "1",
+  "url": "HigherOrder.html",
+  "type": "Chapter",
+  "number": "9",
+  "title": "Higher order ODEs",
+  "body": " Higher order ODEs   Hey kids, want to see an ODE?  It's not so scary if you poke it with a stick.   "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
