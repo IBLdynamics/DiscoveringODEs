e71c6606ea50dfe942552cc4064d70db1929d6cc
Jeffrey Ford <jford1906@gmail.com>
2024-06-03 20:17:32 +0000

    Adjusted initial value for sliders

--- assets/jslibrary/Explore135.js
+++ assets/jslibrary/Explore135.js
@@ -5,55 +5,82 @@ var board = JXG.JSXGraph.initBoard('slope_field_matching', {
     showNavigation:false
   });
   
-  // Slider to control length of vectors
-  var s = board.create('slider', [[0.1, 7.5], [3.1, 7.5], [0, 0.33, 1]], {name: 'length'});
+ 
+// Slider to control length of vectors
+var s = board.create('slider', [
+    [0.1, 7.5],
+    [3.1, 7.5],
+    [0, 0, 1]
+  ], {
+    name: 'length'
+  });
   // Slider to control number of steps
-  var stepsize = board.create('slider', [[0.1, 6.5], [3.1, 6.5], [1, 20, 30]], {name: 'steps', snapWidth: 0.9});
+  var stepsize = board.create('slider', [
+    [0.1, 6.5],
+    [3.1, 6.5],
+    [1, 20, 30]
+  ], {
+    name: 'steps',
+    snapWidth: 0.9
+  });
   
   
   f = function(x, y) {
-            return 0
-            };
-   
+    return 0
+  };
+  
   //initial function for the ODE
   
   
-   // Create buttons
-   var buttona = board.create('button', [-4, 7, 'a',
-      function () {f = function(x, y) {
-            return y*(1-y)
-            };}
-   ], {});
-   var buttonb = board.create('button', [-3, 7, 'b',
-       function () {f = function(x, y) {
-            return Math.sin(x)*y
-            };}
-   ], {});
-   var buttonc = board.create('button', [-2, 7, 'c',
-       function () {f = function(x, y) {
-            return 1 - Math.pow(y,2)
-            };}
-   ], {});
-   var buttond = board.create('button', [-1, 7, 'd',
-       function () {f = function(x, y) {
-            return x*y
-            };}
-   ], {});
+  // Create buttons
+  var buttona = board.create('button', [-4, 7, 'a',
+    function() {
+      f = function(x, y) {
+        s.setValue(0.33);
+        return y * (1 - y)
+      };
+    }
+  ], {});
+  var buttonb = board.create('button', [-3, 7, 'b',
+    function() {
+      f = function(x, y) {
+        s.setValue(0.33);
+        return Math.sin(x) * y
+      };
+    }
+  ], {});
+  var buttonc = board.create('button', [-2, 7, 'c',
+    function() {
+      f = function(x, y) {
+        s.setValue(0.33);
+        return 1 - Math.pow(y, 2)
+      };
+    }
+  ], {});
+  var buttond = board.create('button', [-1, 7, 'd',
+    function() {
+      f = function(x, y) {
+        s.setValue(0.33);
+        return x * y
+      };
+    }
+  ], {});
   
   var field = board.create('slopefield', [
-      (x, y) => f(x,y),
-          [-5, () => stepsize.Value(), 5],
-      [-5, () => stepsize.Value(), 5]],
-      {
-          strokeWidth: 1.5,
-          highlightStrokeWidth: 0.5,
-          highlightStrokeColor: JXG.palette.blue,
+    (x, y) => f(x, y),
+    [-5, () => stepsize.Value(), 5],
+    [-5, () => stepsize.Value(), 5]
+  ], {
+    strokeWidth: 1.5,
+    highlightStrokeWidth: 0.5,
+    highlightStrokeColor: JXG.palette.blue,
   
-          scale: () => s.Value(),
+    scale: () => s.Value(),
   
-          arrowHead: {
-              enabled: true,
-              size: 8,
-              angle: Math.PI / 16
-          }
-      });
+    arrowHead: {
+      enabled: true,
+      size: 8,
+      angle: Math.PI / 16
+    }
+  });
+  
